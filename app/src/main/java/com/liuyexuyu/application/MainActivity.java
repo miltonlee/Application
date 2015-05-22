5b149ea0a1a71ff437ebeab66cc1dd72670900d4
liu'ye'xu'yu <873644479@qq.com>
2015-05-22 22:25:33 +0800

    aaaa

---
+++ app/src/main/java/com/liuyexuyu/application/MainActivity.java
@@ -0,0 +1,16 @@
+package com.liuyexuyu.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("mylog","buzhidao");
+    }
+}
